b7343dd3ce29ea4aecdee4b94d10f641bedd48f2
s-kella <52741545+s-kella@users.noreply.github.com>
2021-01-16 22:38:24 +0300

    Update headhunter.py

--- headhunter.py
+++ headhunter.py
@@ -55,32 +55,6 @@ def find_on_hh():
     vacancies_in_list = []
     for language in languages:
         average, skipped, vacancies_found, vacancies_with_salary = get_info_about_vacancy(payload, url, language)
-        '''page = 0
-        if 'page' in payload:
-            del payload['page']
-        payload['text'] = f'программист {language}'
-        response = requests.get(url, params=payload)
-        response.raise_for_status()
-        pages_number = response.json()['pages']
-        average = 0
-        vacancies_with_salary = 0
-        skipped = 0
-        while page < pages_number:
-            payload['page'] = page
-            page += 1
-            response = requests.get(url, params=payload)
-            response.raise_for_status()
-            vacancies = response.json()['items']
-            for vacancy in vacancies:
-                salary = vacancy['salary']
-                if salary is not None and salary['currency'] == 'RUR':
-                    predict = predict_rub_salary_hh(salary)
-                    average += predict
-                    vacancies_with_salary += 1
-                else:
-                    skipped += 1
-        if vacancies_with_salary != 0:
-            average = int(average / vacancies_with_salary)'''
         languages[language] = {
             'language': language,
             #'vacancies_found': response.json()['found'],
