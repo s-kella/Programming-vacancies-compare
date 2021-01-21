81aec0c7e03021dba6a5855860509df26d5e70ad
s-kella <52741545+s-kella@users.noreply.github.com>
2021-01-21 13:56:37 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,10 +1,18 @@
-def data_to_table(info_about_language, vacancies_in_list):
-    if len(vacancies_in_list) == 0:
-        vacancies_in_list.append(header_table)
-    dict_to_list = []
-    for item in info_about_language:
-        dict_to_list.append(info_about_language[item])
-    vacancies_in_list.append(dict_to_list)
+def join_data_for_table(info_about_language, data_about_languages):
+    if not data_about_languages:
+        data_about_languages.append(header_table)
+    data_about_languages.append([info_about_language[header] for header in info_about_language])
+    return data_about_languages
+
+
+def predict_rub_salary(frm, to):
+    if frm and to:
+        predict = (frm + to)/2
+    elif not frm:
+        predict = to * 0.8
+    elif not to:
+        predict = frm * 1.2
+    return predict
 
 
 languages = {
