57c2445c47422e85cdbf52cb84cec1493d188bce
s-kella <52741545+s-kella@users.noreply.github.com>
2021-01-21 13:57:26 +0300

    Update headhunter.py

--- headhunter.py
+++ headhunter.py
@@ -1,16 +1,6 @@
 import requests
 from terminaltables import SingleTable
-from utils import data_to_table, languages
-
-
-def predict_rub_salary_hh(salary):
-    if salary['from'] != None and salary['to'] != None:
-        predict = (salary['from'] + salary['to'])/2
-    elif salary['from'] is None:
-        predict = salary['to'] * 0.8
-    elif salary['to'] is  None:
-        predict = salary['from'] * 1.2
-    return predict
+import utils
 
 
 def get_info_about_vacancy(payload, url, language):
@@ -33,7 +23,7 @@ def get_info_about_vacancy(payload, url, language):
         for vacancy in vacancies:
             salary = vacancy['salary']
             if salary is not None and salary['currency'] == 'RUR':
-                predict = predict_rub_salary_hh(salary)
+                predict = utils.predict_rub_salary(salary['from'], salary['to'])
                 average += predict
                 vacancies_with_salary += 1
             else:
@@ -52,15 +42,15 @@ def find_on_hh():
         'text': 'программист'
     }
 
-    vacancies_in_list = []
-    for language in languages:
+    data_about_languages = []
+    for language in utils.languages:
         average, skipped, vacancies_found, vacancies_with_salary = get_info_about_vacancy(payload, url, language)
-        languages[language] = {
+        utils.languages[language] = {
             'language': language,
             'vacancies_found': vacancies_found,
             'vacancies_processed': vacancies_with_salary,
             'skipped': skipped,
             'average_salary': average
         }
-        data_to_table(languages[language], vacancies_in_list)
-    return SingleTable(vacancies_in_list, 'HeadHunter Moscow').table
+        data_about_languages = utils.join_data_for_table(utils.languages[language], data_about_languages)
+    return SingleTable(data_about_languages, 'HeadHunter Moscow').table
