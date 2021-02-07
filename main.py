4a8989fd483df8c1215525aa48430e1785391b02
s-kella <52741545+s-kella@users.noreply.github.com>
2021-02-07 13:39:34 +0300

    Environment variables and the list of languages in ifmain

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
 import superjob
 import headhunter
+import os
 from dotenv import load_dotenv
 
 
 if __name__ == '__main__':
     load_dotenv()
-    print(headhunter.find_on_hh())
-    print(superjob.find_on_sj())
+    languages = {
+        'JavaScript': 0,
+        'Java': 0,
+        'python': 0,
+        'ruby': 0,
+        'PHP': 0,
+        'c++': 0,
+        'c#': 0,
+        'c': 0,
+        'go': 0,
+        'Objective-C': 0,
+        'Scala': 0,
+        'Swift': 0,
+        'TypeScript': 0
+    }
+    key_sj = os.getenv("SUPERJOB_KEY")
+    print(superjob.get_data_sj(key_sj, languages))
+    print(headhunter.get_data_hh(languages))
+
 
 
 
